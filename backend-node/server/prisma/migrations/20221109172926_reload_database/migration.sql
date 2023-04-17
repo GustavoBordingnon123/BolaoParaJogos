54b0dae878f4fbbeaccda3e037d180246cbd4605
Gustavo Bordignon <gustavo.bordignon@outlook.com>
2023-04-17 16:28:51 -0300

    first commit

---
+++ backend-node/server/prisma/migrations/20221109172926_reload_database/migration.sql
@@ -0,0 +1,5 @@
+-- DropIndex
+DROP INDEX "Guess_participantId_key";
+
+-- DropIndex
+DROP INDEX "Guess_gameId_key";
---
+++ backend-node/server/src/server.ts
@@ -0,0 +1,35 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
+
+import { poolRoutes } from './routes/pool';
+import { userRoutes } from './routes/user';
+import { gameRoutes } from './routes/game';
+import { guessRoutes } from './routes/guess';
+import { authRoutes } from './routes/auth';
+
+// singleton -> 
+
+async function bootstrap(){
+    const fastify = Fastify({
+        logger:true,
+    })
+
+    await fastify.register(cors, {
+        origin:true,
+    })
+
+    await fastify.register(jwt, {
+        secret: 'segredo'
+    })
+
+    await fastify.register(poolRoutes)
+    await fastify.register(userRoutes)
+    await fastify.register(gameRoutes)
+    await fastify.register(guessRoutes)
+    await fastify.register(authRoutes)
+
+    await fastify.listen({ port: 3333, host: '0.0.0.0' })
+}
+
+bootstrap();
